2f7da856c8fa2a6ab79d113464221f22579dbf42
Brynleigh Payne <bep0022@auburn.edu>
2026-03-25 16:16:06 -0500

    Added all line numbers

--- word_search.py
+++ word_search.py
@@ -5,3 +5,16 @@ if __name__== "__main__": # make modular
     herit_pattern_str = r'\w*herit\w*' # any word including "herit" whether there are any number of letters before or after
     herit_pattern = re.compile(herit_pattern_str, re.IGNORECASE) # ignore upper or lower case
 
+print('Opening origin.txt')
+with open('origin.txt', 'r') as in_stream:
+    print('Opening origin_output.txt')
+    with open('origin_output.txt', 'w') as out_stream:
+        for line_index, line in enumerate(in_stream):
+            line = line.strip()
+            word_list = line.split()
+            word_list.sort()
+            for word in word_list:
+                out_stream.write(f'Line: {line_index + 1}\t{word}\n')
+print("Done!")
+print('origin.txt is closed?', in_stream.closed)
+print('origin_output.txt is closed?', out_stream.closed)
