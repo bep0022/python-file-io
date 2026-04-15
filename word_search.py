9af927339d3085a6221004f8b9ff70c2fd8242f2
Brynleigh Payne <bep0022@auburn.edu>
2026-04-15 12:01:44 -0500

    Closer, but inclduing unwanted puncuation in output

--- word_search.py
+++ word_search.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 import re # import regex module
+
 if __name__== "__main__": # make modular
     herit_pattern_str = r'\w*herit\w*' # any word including "herit" whether there are any number of letters before or after
     herit_pattern = re.compile(herit_pattern_str, re.IGNORECASE) # ignore upper or lower case
@@ -14,7 +15,9 @@ with open('origin.txt', 'r') as in_stream:
             word_list = line.split()
             word_list.sort()
             for word in word_list:
-                out_stream.write(f'Line: {line_index + 1}\t{word}\n')
+                if herit_pattern.search(word):
+                    out_stream.write(f'Line: {line_index + 1}\t{word}\n')
+
 print("Done!")
 print('origin.txt is closed?', in_stream.closed)
 print('origin_output.txt is closed?', out_stream.closed)
